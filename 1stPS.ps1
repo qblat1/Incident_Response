0cab740dc6539bfa1188844b85030bc93ecf5e36
qblat1 <61995436+qblat1@users.noreply.github.com>
2025-09-24 12:59:09 -0700

    Add files via upload
    From CLP class

---
+++ 1stPS.ps1
@@ -0,0 +1,14 @@
+Clear-Host
+$PSDefaultParameterValues['out-file:width']=2000
+Write-Host This is my first powershell script 
+Write-Output ===================================================================================
+Get-Date
+Write-Output ===================================================================================
+Get-TimeZone
+Write-Output ===================================================================================
+Write-Host Most computer problems are due to a loose nut between the computer and the chair!!!
+Write-Output ===================================================================================
+Get-Date
+Write-Output ===================================================================================
+Get-TimeZone
+Write-Output ===================================================================================
